fe96e55b5ac89621bf1956309a1fa8a3f02e2ab5
srishtitalwarr <97547064+srishtitalwarr@users.noreply.github.com>
2024-08-17 16:34:30 +0530

    Game Done
    I shamelessly copied the game from the tutorial repo and got it working. XoX0

---
+++ Dino-Game/src/App.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int boardWidth = 750;
+        int boardHeight = 250;
+
+        JFrame frame = new JFrame("Dino-Game");
+         frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ChromeDinosaur chromeDinosaur = new ChromeDinosaur();
+        frame.add(chromeDinosaur);
+        frame.pack();
+        chromeDinosaur.requestFocus();
+        frame.setVisible(true);
+
+
+    }
+}
